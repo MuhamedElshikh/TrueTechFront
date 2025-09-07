139d58cd437838bfe03f764ce7b288683d58e167
khalidatef <khalidatef202@gmail.com>
2025-09-07 08:21:41 +0300

    fixerror

--- src/app/layout/additions/branch/branch.ts
+++ src/app/layout/additions/branch/branch.ts
@@ -7,22 +7,25 @@ import { RouterLink } from '@angular/router';
   selector: 'app-branch',
   templateUrl: './branch.html',
   styleUrls: ['./branch.css'],
-  imports: [RouterLink]
+  imports: [RouterLink],
 })
 export class Branch implements OnInit {
   branch!: branch[];
-  constructor(private _branchService: Branchservice ) {
-  }
+  constructor(private _branchService: Branchservice) {}
   ngOnInit(): void {
-      this.GetAllBranches();
+    this.GetAllBranches();
   }
-  GetAllBranches() 
-  {
-    this._branchService.getAllBranches().subscribe((res) => {
-      console.log(res);
-console.log('TOKEN =', localStorage.getItem('usertoken'));    
-  this.branch = res;
-    
+  GetAllBranches() {
+    this._branchService.getAllBranches().subscribe({
+      next: (res) => {
+        console.log(res);
+        console.log('TOKEN =', localStorage.getItem('usertoken'));
+        this.branch = res;
+      },
+      error: (err) => {
+        console.error('Error fetching branches:', err);
+        this.branch = [];
+      },
     });
   }
 }
